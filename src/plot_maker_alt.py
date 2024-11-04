84b592988ea09080330e4abcdd65337a9744c34a
Intermaze <rafikiprawhira@gmail.com>
2024-11-04 23:01:01 +0700

    feat: alt title plot maker for SA

---
+++ src/plot_maker_alt.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+y = list(map(int, input("Masukkan nilai y dipisahkan oleh koma (contoh: 1,2,3): ").split(',')))
+x = list(range(1, len(y) + 1))
+
+plt.plot(x, y, marker='o', linestyle='-', color='b')  
+
+plt.xlabel("Jumlah Iterasi")
+plt.ylabel("exp(deltaE/T)")
+plt.title("Plot antara Jumlah Iterasi dan exp(deltaE/T)")
+
+plt.show()

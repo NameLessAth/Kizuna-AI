a7569d4d3ee580ef84233f183a2908968ea4be6a
Intermaze <rafikiprawhira@gmail.com>
2024-11-04 23:01:46 +0700

    fix: float to int err

--- src/plot_maker_alt.py
+++ src/plot_maker_alt.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-y = list(map(int, input("Masukkan nilai y dipisahkan oleh koma (contoh: 1,2,3): ").split(',')))
+y = list(map(float, input("Masukkan nilai y dipisahkan oleh koma (contoh: 1,2,3): ").split(',')))
 x = list(range(1, len(y) + 1))
 
 plt.plot(x, y, marker='o', linestyle='-', color='b')  
